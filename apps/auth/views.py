c7ec8552843d8891f43e75e1deef1d68a4eb0d01
nikolas <nikolassc4mpos@gmail.com>
2025-06-26 14:36:50 -0300

    fix: images

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 
-def signup(request):    
+@login_required
+def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
 from .forms import BotanicalForm
 from .models import BotanicalRegister
 
+@never_cache
+@login_required
 def create(request):
     if request.method == 'POST':
         form = BotanicalForm(request.POST, request.FILES)
@@ -12,11 +16,14 @@ def create(request):
         form = BotanicalForm()
     return render(request, 'core/register_botanical.html', {'form': form, 'is_update': False })
 
+@never_cache
+@login_required
 def read(request):
     botanicals = BotanicalRegister.objects.all()
     return render(request, 'core/catalog.html', {'botanicals': botanicals})
 
-
+@never_cache
+@login_required
 def update(request, id):
     plant = get_object_or_404(BotanicalRegister, id=id)
 
@@ -34,11 +41,15 @@ def update(request, id):
         'plant': plant
     })
 
+@never_cache
+@login_required
 def delete(request, id):
     plant = get_object_or_404(BotanicalRegister, id=id)
     plant.delete()
     return redirect('catalog')
 
+@never_cache
+@login_required
 def view(request, id):
     plant = get_object_or_404(BotanicalRegister, id=id)
     return render(request, 'core/view_botanical.html', {'plant': plant})
